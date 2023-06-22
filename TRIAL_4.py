0a400e07ca97fa95a0c6a4d4794dfab1dabfe0bc
StevenGbena <136390353+StevenGbena@users.noreply.github.com>
2023-06-22 09:50:44 +0100

    Add files via upload

---
+++ TRIAL_4.py
@@ -0,0 +1,24 @@
+
+
+
+
+
+user=int(input('enter the number :'))
+num=0
+
+while num<1:
+    if user %2==1:
+     print ((user),'is a prime number')
+     print('and the square root is:')
+     import math 
+     print(math.sqrt(user))
+     break 
+    else:
+       print('it is not a prime number')
+     
+       print(user=int(input('enter the number:')))
+    num=num+2
+        
+
+
+
